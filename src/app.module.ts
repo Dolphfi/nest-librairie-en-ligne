7f2f726fed02dc1ae6c2e03ac63d5af3cb571b9e
Rodolph <rodolphfidele@gmail.com>
2023-08-24 18:41:51 -0400

    Data_base_create

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthentModule } from './authent/authent.module';
 import { LivresModule } from './livres/livres.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AuthentModule, LivresModule],
-  controllers: [AppController],
+  imports: [
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    database: 'bibliotheque',
+    username: 'root',
+    password: 'Rodolph4904@',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  AuthentModule, 
+  LivresModule
+],
   providers: [AppService],
 })
 export class AppModule {}
