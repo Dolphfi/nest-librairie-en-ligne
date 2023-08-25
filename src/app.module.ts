2707815f19c5ff2f7c8cd4ad362d692c399cc73d
Rodolph <rodolphfidele@gmail.com>
2023-08-25 14:03:20 -0400

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthentModule } from './authent/authent.module';
 import { LivresModule } from './livres/livres.module';
