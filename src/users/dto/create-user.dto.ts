1a1a4180c696a432fef3ce15bbe59629a7173766
Rodolph <rodolphfidele@gmail.com>
2023-08-25 15:34:22 -0400

    User...

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({message:'Le nom est obligatoire'})
+    nom: string;
+    @IsNotEmpty({message:'Le prénom est obligatoire'})
+    prenom: string;
+    @IsNotEmpty({message:'L\'email est obligatoire'})
+    @IsEmail({},{message:'L\'email n\'est pas valide'})
+    email: string;
+    @IsNotEmpty({message:'Le nom utilisateur est obligatoire'})
+    @MinLength(3,{message:'Le nom utilisateur doit faire au moins 5 caractéres'})
+    username: string;
+    @IsNotEmpty()
+    @MinLength(8,{message:'Le mot de passe doit faire au moins 8 caractéres'})
+    password: string;
+    @IsNotEmpty()
+    @MinLength(8,{message:'Le mot de passe doit faire au moins 8 caractéres'})
+    confirm_password: string;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({ nullable: false })
+    nom: string;
+    @Column({ nullable: false })
+    prenom: string;
+    @Column({ unique: true, nullable: false })
+    email: string;
+    @Column({ unique: true, nullable: false })
+    username: string;
+    @Column({ nullable: false })
+    password: string;
+    @Column({nullable: false, default: 1 })
+    isActive: boolean;
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+}
