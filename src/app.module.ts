1ec426a9db91eaa7dabfc1efe48a1fa44f03c610
Myrtil <manorahsaintfort88@gmail.com>
2023-08-25 12:31:07 -0400

    La creation du module Utilisateur

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthentModule } from './authent/authent.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthentModule],
+  imports: [AuthentModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
