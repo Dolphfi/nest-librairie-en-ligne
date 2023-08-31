e0fcdb2322ac01b8051edc2587447a55177089c0
Myrtil <manorahsaintfort88@gmail.com>
2023-08-31 16:46:53 -0400

    .

--- src/livres/livres.controller.ts
+++ src/livres/livres.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { LivresService } from './livres.service';
 import { CreateLivreDto } from './dto/create-livre.dto';
 import { UpdateLivreDto } from './dto/update-livre.dto';
+import { Livre } from './entities/livre.entity';
+import { AuthentificationGuard } from 'src/utility/guards/authentification.guard';
+import { AuthorizeGuard } from 'src/utility/guards/authorization.guard';
+import { CurrentUser } from 'src/utility/decorators/current-users.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { AuthorizeRoles } from 'src/utility/decorators/authorize-roles.decorator';
+import { UserRole } from 'src/utility/common/user-roles.enum';
 
 @Controller('livres')
 export class LivresController {
@@ -13,18 +20,21 @@ export class LivresController {
   }
 
   @Get()
-  findAll() {
-    return this.livresService.findAll();
+  async findAll():Promise <Livre[]> {
+    return await this.livresService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.livresService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.livresService.findOne(+id);
   }
 
+  @AuthorizeRoles(UserRole.Admin)
+  @UseGuards(AuthentificationGuard,AuthorizeGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLivreDto: UpdateLivreDto) {
-    return this.livresService.update(+id, updateLivreDto);
+  async update(@Param('id') id: string, @Body() updateLivreDto: UpdateLivreDto,@CurrentUser()
+  CurrentUser:User) {
+    return await this.livresService.update(+id, updateLivreDto,CurrentUser);
   }
 
   @Delete(':id')
