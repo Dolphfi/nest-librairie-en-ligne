093807e7790ac62ccb5eb53a88fe5ba92285d015
Rodolph <rodolphfidele@gmail.com>
2023-08-31 16:24:01 -0400

    .

--- src/livres/livres.module.ts
+++ src/livres/livres.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LivresService } from './livres.service';
 import { LivresController } from './livres.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Livre } from './entities/livre.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Livre])],
   controllers: [LivresController],
   providers: [LivresService],
 })
